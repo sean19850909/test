77806ff717287987236c99bc44e59bd2375a654b
主文翔 <sean@zhuwenxiangdeMacBook-Pro.local>
2018-03-28 16:41:31 +0800

    config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '198.35.47.56'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('reading', 'forge'),
+            'username' => env('root', 'forge'),
+            'password' => env('1q2w3e4r', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
